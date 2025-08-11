0efee878648bfc99edb0c3129d585ab553173087
Darpan <bhamredarpan@gmail.com>
2025-08-12 01:34:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './Components/Navbar'
+
+function App() {
+
+
+  return (
+    <>
+     <Navbar title="TextUtils2" aboutText="About Us"/>
+     {/* here variables title and aboutText are passed as props.  */}
+    </>
+  )
+}
+
+export default App
