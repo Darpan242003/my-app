40434abe89f8f936ff194b9b737b3f5c9ba4f575
Darpan Bhamre <bhamredarpan@gmail.com>
2025-08-27 04:09:42 +0530

    App Deployed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,18 +7,18 @@ const Navbar = (props) => {
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.navbarBg}`}>{/*change the navbar-light and bg-light properties with props to change light/dark using javascript template literals.*/}
       <div className="container-fluid">
         {/* title variable as prop */}
-        <a className="navbar-brand" href="/">{props.title}</a>
+        <a className="navbar-brand" href="#">{props.title}</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">Home</a>
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
             </li>
             <li className="nav-item">
               {/* aboutText variable as prop */}
-              <a className="nav-link" href="/">{props.aboutText}</a>
+              <a className="nav-link" href="#">{props.aboutText}</a>
             </li>
           </ul>
           {/* <form className="d-flex" role="search">
