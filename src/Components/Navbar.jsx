e3676461592bf81728cb0bc4575ff054d1118bab
Darpan Bhamre <bhamredarpan@gmail.com>
2025-08-27 02:55:30 +0530

    Removed React Router

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
 
 //Here props are accepted in our Navbar component
 const Navbar = (props) => {
@@ -8,18 +7,18 @@ const Navbar = (props) => {
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.navbarBg}`}>{/*change the navbar-light and bg-light properties with props to change light/dark using javascript template literals.*/}
       <div className="container-fluid">
         {/* title variable as prop */}
-        <Link className="navbar-brand" to="/">{props.title}</Link>
+        <a className="navbar-brand" href="/">{props.title}</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+              <a className="nav-link active" aria-current="page" href="/">Home</a>
             </li>
             <li className="nav-item">
               {/* aboutText variable as prop */}
-              <Link className="nav-link" to="/about">{props.aboutText}</Link>
+              <a className="nav-link" href="/">{props.aboutText}</a>
             </li>
           </ul>
           {/* <form className="d-flex" role="search">
@@ -57,4 +56,3 @@ Navbar.defaultProps = {
 
 
 export default Navbar
-
